13811fa65210c18607e5dc7c85afa3477f1b1ff2
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-02 15:39:15 -0400

    fix lint

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature "UserAuths", type: :feature do
+RSpec.feature 'UserAuths', type: :feature do
   let(:user) { create(:user) }
 
-  scenario "user signs up" do
+  scenario 'user signs up' do
     visit new_user_registration_path
     # Fill in the sign-up form and submit
     # Add assertions to verify a new user was created
   end
 
-  scenario "user signs in" do
+  scenario 'user signs in' do
     # Add tests for user sign-in
   end
 
-  scenario "user signs out" do
+  scenario 'user signs out' do
     # Add tests for user sign-out
   end
 
