dbc9e17ae868d8f6719bdde03f9e6d613ca69ab4
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-02 15:35:43 -0400

    write new test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
+
+  it 'is valid with valid attributes' do
+    user = User.new(name: 'John Doe', email: 'john@example.com')
+    expect(user).to be_valid
+  end
 end
