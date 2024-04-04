564dcd0239fe3a8e61a47ad58a4eb786f443f7d4
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-04 14:09:12 -0400

    add role to users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,17 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
+
+    trait :admin do
+      role { :admin }
+    end
+
+    trait :therapist do
+      role { :therapist }
+    end
+
+    trait :client do
+      role { :client }
+    end
   end
 end
