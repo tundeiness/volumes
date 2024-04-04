296184596533e27ce6f0113d10b2f9aec62f33d3
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-04 14:45:58 -0400

    implement admin sign in

--- spec/features/admin_auth.rb
+++ spec/features/admin_auth.rb
@@ -4,20 +4,9 @@ RSpec.feature 'Admin Sign In', type: :feature do
   scenario 'Admin can sign in with valid credentials' do
     admin = create(:user, :admin)
     visit new_user_session_path
-    fill_in 'admin[email]', with: admin.email
-    fill_in 'user[password]', with: admin.password
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
     click_button 'Log in'
     expect(page).to have_text('Signed in successfully.')
   end
-
-  # scenario 'user signs in' do
-  #   user = create(:user)
-  #   visit new_user_session_path
-
-  #   fill_in 'user[email]', with: user.email
-  #   fill_in 'user[password]', with: user.password
-  #   click_button 'Log in'
-
-  #   expect(page).to have_content('Signed in successfully.')
-  # end
 end
