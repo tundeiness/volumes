a58a1a9852439b4556f4f6caa0fd7cc5997ae11c
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-04 14:45:22 -0400

    edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-  enum role: %i[admin double single]
+  ROLES = %w[admin therapist client].freeze
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attribute :role, :string, default: 'client'
+
+  ROLES.each do |name|
+    define_method "#{name}?" do
+      role == name
+    end
+  end
 end
