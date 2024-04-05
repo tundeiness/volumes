494614ac963ef440d9aada579160b5c95da353e3
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-05 16:41:21 -0400

    add attributes to user factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    contact_number { Faker::PhoneNumber.phone_number }
+    address { Faker::Address.full_address }
+    username { Faker::Movies::LordOfTheRings }
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
+    gender { Faker::Gender }
 
     trait :admin do
       role { :admin }
