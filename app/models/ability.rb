e6c7e4f53a779e7b6cbdb84f958024df43510f12
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-04 23:08:31 -0400

    edit ability.rb

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    elsif user.therapist?
+      can :manage, Appointment, therapist_id: user.id
+      can :read, Client
+    elsif user.client?
+      # can :create, Appointment
+      # can :read, Appointment, client_id: user.id
+    end
+  end
+end
