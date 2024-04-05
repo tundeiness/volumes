f38cea59b26ae5d44da388a2b43993927c2fd8cb
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-05 16:07:01 -0400

    add attributes to user schema

--- db/migrate/20240405195810_add_custom_fields_to_users.rb
+++ db/migrate/20240405195810_add_custom_fields_to_users.rb
@@ -1,4 +1,10 @@
 class AddCustomFieldsToUsers < ActiveRecord::Migration[7.1]
   def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :contact_number, :string
+    add_column :users, :address, :string
+    add_column :users, :username, :string
+    add_column :users, :gender, :string
   end
 end
