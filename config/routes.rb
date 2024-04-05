e806e66b90a63678d95d663af1b05eb13e8d038b
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-04 23:09:36 -0400

    add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/update'
   devise_for :users
   # devise_for :users, controllers: { registrations: 'user/registrations' }
   root 'home#index'
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :users, only: [:index, :edit, :update]
 end
