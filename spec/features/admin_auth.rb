1c3b4f71fad3596788b14a59515cc3b765353d20
Tunde Oretade <tundeiness@yahoo.co.uk>
2024-04-05 10:49:57 -0400

    comment admin auth

--- spec/features/admin_auth.rb
+++ spec/features/admin_auth.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.feature 'Admin Sign In', type: :feature do
-  scenario 'Admin can sign in with valid credentials' do
-    admin = create(:user, :admin)
-    visit new_user_session_path
-    fill_in 'Email', with: admin.email
-    fill_in 'Password', with: admin.password
-    click_button 'Log in'
-    expect(page).to have_text('Signed in successfully.')
-  end
-end
+# RSpec.feature 'Admin Sign In', type: :feature do
+#   scenario 'Admin can sign in with valid credentials' do
+#     admin = create(:user, :admin)
+#     visit new_user_session_path
+#     fill_in 'Email', with: admin.email
+#     fill_in 'Password', with: admin.password
+#     click_button 'Log in'
+#     expect(page).to have_text('Signed in successfully.')
+#   end
+# end
